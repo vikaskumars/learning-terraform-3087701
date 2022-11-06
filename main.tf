1a14f7f9d2c16a19a14ddf5494d901bf1f17bcca
vikaskumars <vikas_aieee2001@yahoo.com>
2022-11-06 18:13:05 +0530

    minor change

--- main.tf
+++ main.tf
@@ -45,8 +45,8 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.0"
+  
   name = "blog"
-
   vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
