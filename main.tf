933e3b8100ea9b3c873be595838f7bf81efeb08e
vikaskumars <vikas_aieee2001@yahoo.com>
2022-11-06 18:26:36 +0530

    added subnet

--- main.tf
+++ main.tf
@@ -35,6 +35,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   
+  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
